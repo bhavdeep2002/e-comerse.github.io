e7a137e9dcbe126ceef77483e3ad5048520853f5
Bhavdeep Singh <78559880+bhavdeep2002@users.noreply.github.com>
2023-08-14 15:44:57 +0530

    change

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,28 @@
-function filter(){
-    var lowerlimit= document.getElementById("l").value;
-    var upperlimit= document.getElementById("up").value;
-     const a =[2,30,40,60,20];
-    for(var i=0;i<a.length;i++){
-        if(a[i]>=lowerlimit && a[i]<=upperlimit){
-            document.write(a[i]+"<br>");
+function search(){
+    var search =document.getElementById("in").value;
+    var productlist=document.getElementsByClassName("card");
+    var producttitle=document.getElementsByClassName("card-title");
+    var found =0;
+    var index;
+    var count=0;
+  for(var i=0;i<productlist.length;i++){
+    if(producttitle[i].innerHTML==search){
+        index=i;
+        found =1;
+        break;
+      }
+  }
+  if(found==1){
+    for(var j=0;j<productlist.length;j++){
+        if(index==j){
+            productlist[j].style.display="block";
+        }
+        else{
+            productlist[j].style.display="none";
         }
     }
+  }
+  else{
+       document.write("Search not found");
+  }
 }
