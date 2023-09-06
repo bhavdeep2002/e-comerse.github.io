4b3b44e2c68256ff7ee879646c5d2c32a83df640
Bhavdeep Singh <78559880+bhavdeep2002@users.noreply.github.com>
2023-09-07 01:06:50 +0530

    search filter

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -126,13 +126,53 @@ sort(){
 }
 }
 
- const search=() =>{
+ 
+const searchproduct =()=>{
+    let input =document.getElementById("in").value.trim()
+    let card =document.getElementsByClassName("card")
+    let cardname =document.getElementsByClassName("card-title")
+
+    for (let i = 0; i < card.length; i++) {
+        if(cardname[i].textContent.toUpperCase().trim()===input){
+            let temp;
+            temp =card[i].innerHTML
+            card[i].innerHTML=card[0].innerHTML
+            card[0].innerHTML=temp
+             
+        }
+        
+    }
+}
+
+const search=() =>{// no 2: for filtering the search as typing
+    listbar()
     let input =document.getElementById("in").value.toUpperCase()
     let list =document.querySelectorAll(".list")
-    
+
    for (let i = 0; i < list.length; i++) {
-         
-        if(list[i].innerHTML.toUpperCase().trim().includes(input)){
+         let li =list[i].textContent.toUpperCase().trim()// li contains textcontent of list[i]
+        
+         if(li.indexOf(input)>-1)
+        {   
+            let stringcolored =""
+            let colored =li.indexOf(input)
+          for(let i=0;i<li.length;i++){
+            if(li[i]===li[colored]){
+                stringcolored+="<span class='char'>" +li[colored] +'</span>'
+            }
+            else{
+                stringcolored+=li[i];
+            }
+
+          }
+
+          if(input===""){
+            list[i].innerHTML=list[i].innerHTML
+          }
+          else{
+            list[i].innerHTML=stringcolored
+          }
+
             list[i].style.display='';
         }
         else{
@@ -143,12 +183,36 @@ sort(){
 
     }
 
-    const listbar=()=>{
-        console.log("list")
-        let list =document.getElementById("my-ul")
-        list.style.display="block"
+    {//no 4: for clicking a list element and adding their innerText in input
+    
+    let input= document.getElementById("in")
+    let list= document.querySelectorAll(".list")
+    let ul =document.getElementById("my-ul")
 
+    list.forEach(name => {
+      
+     name.addEventListener('click',inser=()=>{
+         input.value=name.innerText
+         ul.style.display="none"
+     })
+    });
+}
+
+
+    const listbar =()=>{// no 1: for showing list
+        let input =document.getElementById("in").value.trim()
+        if(input===""){
+            document.getElementById("my-ul").style.display="none"
+        }
+
+        else{
+            document.getElementById("my-ul").style.display="block"
+        }
+        
     }
+
+   
+
 
 
 {
@@ -169,7 +233,7 @@ sort(){
     else{
         let groceries =[new product("Assorted Coffee",19.00,"Groceries","image/coffee.PNG","groceries"),new product("Fresh Orange Juice",19.00,"Groceries","image/orangejuice.PNG","groceries"),new product("Hand Sanitizer",19.00,"Groceries","image/handsanatizer.PNG","groceries")]
 
-        groceries.push(new product("Cashew Butter",19.00,"Groceries","image/casheo.PNG","groceries"),new product("Diabetic Cookies",25.00,"Groceries","image/cookies.PNG","groceries"),new product("Fresh Organic Honey",34.00,"Groceries","image/honey.PNG","groceries"))
+        groceries.push(new product("Cashew Butter",19.00,"Groceries","image/casheo.PNG","groceries"),new product("Diabetic Cookies",25.00,"Groceries","image/cookies.PNG","groceries"),new product("Fresh Organic Honey",34.00,"Groceries","image/honey.PNG","groceries"),new product("Organic Face Scrub",35.00,"Groceries","image/facescrub.PNG","groceries"),new product("Pulses From Organic Farm",15.00,"Groceries","image/dal.PNG" ,"groceries"),new product("Natural Extracted Edible Oil",19.00,"Groceries","image/oil.PNG","bestselling"))
 
         groceries.forEach(product => {
             console.log(product)
